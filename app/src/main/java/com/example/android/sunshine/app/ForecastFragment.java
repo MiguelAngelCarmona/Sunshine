00821c1b11a241447b41e25692cffa5852284383
MIGUELANGEL CARMONA <miki.mikicm@gmail.com>
2014-09-19 11:24:50 +0200

    Settings Units...Failure starting...fahrenheit, not celsius

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -133,7 +133,18 @@ public class ForecastFragment extends Fragment {
         }
 
         private String formatHighLows(double high, double low) {
-// For presentation, assume the user doesn't care about tenths of a degree.
+// For presentation, assume the user doesn't care about tenths of a degree. Celsius degrees by default
+            SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPreferences.getString(getString(R.string.pref_units_key),getString(R.string.pref_units_metric));
+            //If imperial we parse to Fahrenheit
+
+            if (unitType.equals(getString(R.string.pref_units_imperial))){
+                high=(high*1.8)+32;
+                low=(low*1.8)+32;
+            }else if (!unitType.equals(getString(R.string.pref_units_metric))){
+                Log.d(LOG_TAG,"Unit type not found: "+unitType);
+            }
+
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
             String highLowStr = roundedHigh + "/" + roundedLow;
